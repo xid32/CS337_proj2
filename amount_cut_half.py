2d1d395f2f023a48b12ec940ebd0db6b31801841
Yuuuujia <58340202+Yuuuujia@users.noreply.github.com>
2021-03-05 09:39:07 +0800

    Add files via upload

---
+++ amount_cut_half.py
@@ -0,0 +1,44 @@
+from unidecode import unidecode
+from fractions import Fraction
+import copy
+import json
+
+
+def amount_cut_half():
+
+    f = open('recipe_representation.json')
+    rep = json.load(f)
+
+    ingradient_list = rep['ingredients']
+    ingradient_half = copy.deepcopy(ingradient_list)
+
+    for i in range(len(ingradient_list)):
+        get_quantity = unidecode(ingradient_list[i]['quantity']).split()
+        if len(get_quantity) > 0:
+            if len(get_quantity) == 1:
+                quantity_num = float(Fraction(get_quantity[0]))
+            else:
+                quantity_num = float(Fraction(get_quantity[0]) + Fraction(get_quantity[1]))
+
+            if len(ingradient_list[i]['measurement']) != 0:
+                quantity_half = quantity_num / 2
+            else:
+                quantity_half = quantity_num // 2
+            ingradient_half[i]['quantity'] = str(quantity_half)
+
+            print(ingradient_list[i]['ingredient_name'] + ' cut by half, from ' + str(quantity_num) + ' to ' + str(quantity_half))
+
+    print('All ingredients cut by half! Methods, tools and directions remains the same.')
+
+    recipe_double = {}
+    recipe_double['directions'] = rep['directions']
+    recipe_double['ingredients'] = ingradient_half
+    recipe_double['methods'] = rep['methods']
+    recipe_double['tools'] = rep['tools']
+
+    # Save new recipe as a file
+    with open('recipe_representation.json', 'w') as fp:
+        json.dump(recipe_double, fp, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    amount_cut_half()
